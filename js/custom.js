f7ff8d928cde7d336d81d25a5e3cce243a3be55e
Nav-preet <navmashiana60@gmail.com>
2021-02-27 13:38:27 +0530

    Done with the functinality of car game

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 const text = document.querySelector(".text");
 const gameArea = document.querySelector(".gameArea");
-let player = { speed: 5 }
+let player = { speed: 3 }
 let keys = { ArrowUp : false, ArrowDown : false, ArrowLeft : false, ArrowRight : false }
 text.addEventListener('click',start);
 
@@ -31,6 +31,26 @@ function start(){
     car.setAttribute('class','car');
     gameArea.appendChild(car);
 
+    for (i=0; i<5 ; i++){
+
+        let roadLines = document.createElement('div');
+        roadLines.setAttribute('class' , 'lines');
+        roadLines.y = (i*150);
+        roadLines.style.top = roadLines.y + 'px';
+        gameArea.appendChild(roadLines);
+    }
+
+    for (x=0; x<3 ; x++){
+
+        let roadCars = document.createElement('div');
+        roadCars.setAttribute('class' , 'enemyCars');
+        roadCars.y = ((x+1)*350)*-1;
+        roadCars.style.top = roadCars.y + 'px';
+        roadCars.style.background = 'blue';
+        roadCars.style.left = Math.floor(Math.random() * 350) + "px";
+        gameArea.appendChild(roadCars);
+    } 
+
     player.start = true ;
     player.x = car.offsetTop;
     player.y = car.offsetLeft;
@@ -38,18 +58,56 @@ function start(){
     console.log(player.y);
     window.requestAnimationFrame(gamePlay);
 }
+function isCollide(a,b){
+    aPos = a.getBoundingClientRect();
+    bPos = b.getBoundingClientRect();
+    
+    return !((aPos.top > bPos.bottom) || (aPos.bottom < bPos.top) || (aPos.left > bPos.right) || (aPos.right < bPos.left))
+}
 
+function moveLines(){
+    let lines = document.querySelectorAll('.lines');
+    
+    lines.forEach(function(item) {
+        if(item.y >= 700){
+            item.y -= 750;
+        }
+        item.y += player.speed;
+        item.style.top = item.y + 'px';
+    });
+} 
+
+function moveCars(car){
+    let eCars = document.querySelectorAll('.enemyCars');
+    
+    eCars.forEach(function(item) {
+         if(isCollide(car, item)){
+            console.log('hit');
+        }
+        if(item.y >= 750){
+            item.y = -300;
+            item.style.left = Math.floor(Math.random() * 350) + "px";
+        }
+        item.y += player.speed;
+        item.style.top = item.y + 'px';
+    });
+} 
 
 function gamePlay(){
     let car = document.querySelector(".car");
+    let eCars = document.querySelector('.enemyCars');
     let road = gameArea.getBoundingClientRect();
-    console.log(road);
+    
+    moveLines();
+    moveCars(car);
+
+    
 
     if(player.start){
-        if(keys.ArrowUp){ player.x -= player.speed ; }
-        if(keys.ArrowDown){ player.x += player.speed ; }
-        if(keys.ArrowLeft){ player.y -= player.speed ; }
-        if(keys.ArrowRight){ player.y += player.speed ; }
+        if(keys.ArrowUp && player.x > (road.top + 50)) { player.x -= player.speed ; }
+        if(keys.ArrowDown && player.x < (road.bottom - 70)){ player.x += player.speed ; }
+        if(keys.ArrowLeft && player.y > 0){ player.y -= player.speed ; }
+        if(keys.ArrowRight && player.y < (road.width - 50)){ player.y += player.speed ; }
          car.style.top = player.x + "px";
          car.style.left = player.y + "px";        
     }
